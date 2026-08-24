a248273172a94ec69edbe4924863bc5aac5efc06
nazharmunir <mazhar.munir1233@gmail.com>
2026-08-24 16:05:23 +0200

    feat: switch feature branch to live integration dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import LiveDashboard from "./LiveDashboard";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <LiveDashboard />
   </StrictMode>,
 );
